e2e26d445939ac89281395cbe1055962c8416524
Scriptgadget Support <scriptgadget@gmail.com>
2011-01-27 21:53:53 -0800

    Just experimenting and filling in gaps. The site hasn't gelled for me yet.

--- server/ncm/ncm.py
+++ server/ncm/ncm.py
@@ -1,5 +1,6 @@
 # !/usr/bin/env python
 import os
+import logging
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
@@ -10,23 +11,51 @@ from model import Maker, Product
 from gaesessions import get_current_session
 
 class AuthenticatedPage(webapp.RequestHandler):
+    def getMakerForUser(self, user):
+        """ get the Maker if any associated with this user """
+        user_id = user.user_id()
+        maker = None
+
+        try:
+            makers = Maker.gql("WHERE user_id = :1", user.user_id())
+            maker = makers.get()
+        except db.KindError:
+            maker = None
+            logging.debugging.error("Unexpected db.KindError: " + db.KindError);
+        return maker;
+
     def authenticate(self):
         """ Ask a visitor to login before proceeding.  """
         user = users.get_current_user()
-        
         user_id = None
         maker = None
+
         if not user:
             self.redirect(users.create_login_url(self.request.uri))
         else:
-            user_id = user.user_id()   
-            try:
-                makers = Maker.gql("WHERE user_id = :1", user.user_id())
-                maker = makers.fetch(1)
-            except db.KindError:
-                maker = None
-        
+            maker = self.getMakerForUser(user)
+
         return (user_id, maker)
+
+class Login(AuthenticatedPage):
+    """ Just authenticates then redirects to the home page """
+    def get(self):
+        (user_id, maker) = self.authenticate()
+        logging.info('user_id: ' + str(user_id) + ' maker: ' + str(maker));
+        if maker:
+            session = get_current_session()
+            session.start(ssl_only=True)
+            session.regenerate_id()
+            self.redirect("/maker_store")
+        else:
+            self.redirect("/register_maker")
+
+class Logout(AuthenticatedPage):
+    """ Just kills the session and clears authentication tokens  """
+    def get(self):
+        session = get_current_session()
+        session.terminate()
+        self.redirect(users.create_login_url('/'))
 
 class MakerDashboard(AuthenticatedPage):
     """ Renders a page for Makers to view and manage their catalog and sales """
@@ -46,23 +75,27 @@ class MakerDashboard(AuthenticatedPage):
         path = os.path.join(os.path.dirname(__file__), "templates/maker_dashboard.html")
         self.response.out.write(template.render(path, template_values))
 
-class ShopperRegistrationPage(webapp.RequestHandler):
-    """ Renders the new user registration template."""    
-    def get(self):
-        template_values = { 'title':'Register'}
-        path = os.path.join(os.path.dirname(__file__), "templates/register_shopper.html")
-        self.response.out.write(template.render(path, template_values))
-
-class MakerRegistrationPage(webapp.RequestHandler):
+class MakerRegistrationPage(AuthenticatedPage):
     """ Renders the new maker registration template."""    
     def get(self):
-        template_values = { 'title':'Open Your Store'}
+        (user_id, maker) = self.authenticate()
+
+        session = get_current_session()
+        session.start(ssl_only=True)
+        alerts = session.get('alerts', '')
+        template_values = { 'title':'Open Your Store', 'alerts': alerts, 'maker':maker}
+        session['alerts'] = ''
         path = os.path.join(os.path.dirname(__file__), "templates/register_maker.html")
+        logging.info("Showing Registration Page")
         self.response.out.write(template.render(path, template_values))
 
     def post(self):
+        """ Add a new Maker to the community."""
         try:
-            maker = Maker();
+            (user_id, maker) = self.authenticate()
+            if maker is None and user_id is not None:
+                maker = Maker()
+                maker.user_id = user_id
             maker.store_name = self.request.get('store_name')
             maker.store_description = self.request.get('store_description')
             maker.location = self.request.get('location')
@@ -72,24 +105,30 @@ class MakerRegistrationPage(webapp.RequestHandler):
             if value.count('@') == 1:
                 maker.paypal = value
             else:
-                maker.paypal = maker.email
-                
-                maker.phone = self.request.get('phone')
-                maker.mailing_address = self.request.get('mailing_address')
-                maker.put()
-                self.redirect('/maker_store/'+str(maker.key()))
+                maker.paypal = maker.email                
+            maker.phone_number = self.request.get('phone_number')
+            maker.mailing_address = self.request.get('mailing_address')
+            maker.put()
+            logging.debug("Adding or updating  maker == " + maker.full_name)
+            self.redirect('/maker_store/'+str(maker.key()))
         except :
+            session = get_current_session()
+            session['alerts'] = 'An error occured. Please try again'
             self.redirect(self.request.uri)
 
-class HomePage(webapp.RequestHandler):
+class HomePage(AuthenticatedPage):
     """ Renders the home page template. """
     def get(self):
+        user = users.get_current_user()
+        maker = None
+        if user is not None:
+            maker = self.getMakerForUser(user)
         q = Product.all()
         results = q.fetch(3)
         stuff = []
         for p in results:
             stuff.append(p)
-        template_values = { 'title':'Nevada County Makes', 'stuff':stuff}
+        template_values = { 'title':'Nevada County Makes', 'stuff':stuff, 'maker':maker }
         path = os.path.join(os.path.dirname(__file__), "templates/home.html")
         self.response.out.write(template.render(path, template_values))
 
@@ -126,8 +165,11 @@ class ProductPage(webapp.RequestHandler):
 def main():
     app = webapp.WSGIApplication([
         ('/', HomePage),
+        ('/home', HomePage),
         ('/privacy', PrivacyPage),
         ('/terms', TermsPage),
+        ('/login', Login),
+        ('/logout', Logout),
         (r'/maker_store/(.*)', MakerStorePage),
         (r'/maker_dashboard/(.*)', MakerDashboard),
         (r'/product/(.*)', ProductPage),
---
+++ server/ncm/static/async_utils.js
@@ -0,0 +1,91 @@
+
+//
+// As mentioned at http://en.wikipedia.org/wiki/XMLHttpRequest
+//
+if( !window.XMLHttpRequest ) XMLHttpRequest = function()
+    {
+		try{ return new ActiveXObject("Msxml2.XMLHTTP.6.0") }catch(e){}
+		try{ return new ActiveXObject("Msxml2.XMLHTTP.3.0") }catch(e){}
+		try{ return new ActiveXObject("Msxml2.XMLHTTP") }catch(e){}
+		try{ return new ActiveXObject("Microsoft.XMLHTTP") }catch(e){}
+		throw new Error("Could not find an XMLHttpRequest alternative.")
+	};
+
+//
+// Makes an AJAX request to a local server function w/ optional arguments
+//
+// functionName: the name of the server's AJAX function to call
+// opt_argv: an Array of arguments for the AJAX function
+//
+function Request(idempotent, function_name, opt_argv) {
+
+	if (!opt_argv)
+        opt_argv = new Array();
+
+	// Find if the last arg is a callback function; save it
+	var callback = null;
+	var len = opt_argv.length;
+	if (len > 0 && typeof opt_argv[len-1] == 'function') {
+        callback = opt_argv[len-1];
+        opt_argv.length--;
+	}
+	var async = (callback != null);
+
+	// Encode the arguments in to a URI
+	var query = 'action=' + encodeURIComponent(function_name);
+	for (var i = 0; i < opt_argv.length; i++) {
+        var key = 'arg' + i;
+        var val = JSON.stringify(opt_argv[i]);
+        query += '&' + key + '=' + encodeURIComponent(val);
+	}
+	query += '&time=' + new Date().getTime(); // IE cache workaround
+      
+	var req = new XMLHttpRequest();
+	if(idempotent){
+        req.open('GET', '/rpc?' + query, async);
+	}else{
+        req.open('POST', '/rpc', async);
+        req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        req.setRequestHeader("Content-length", query.length);
+        req.setRequestHeader("Connection", "close");        
+	}
+
+	if (async) {
+        req.onreadystatechange = function() {
+			if(req.readyState == 4 && req.status == 200) {
+				var response = null;
+				try {
+					response = JSON.parse(req.responseText); 
+				} catch (e) {
+					$('alert1').innerHTML = "Error: " + req.responseText;
+				}
+				callback(response);
+			}
+
+			if(req.status != 200){
+				$('alert1').innerHTML = "Server Error: " + req.status;
+			}
+        }
+	}
+      
+	// Make the actual request
+	if(idempotent){
+        req.send(null);
+	}else{
+        req.send(query);
+	}
+}
+
+// Adds a stub function that will pass the arguments to the AJAX call
+function InstallFunction(obj, functionName, idempotent) {
+	obj[functionName] = function() { Request(idempotent, functionName, arguments); }
+}
+
+// Server object that will contain the callable methods
+var server = {};
+
+// Handy "macro"
+function $(id){
+	return document.getElementById(id);
+}
+
