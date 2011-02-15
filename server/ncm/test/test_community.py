e8e99e0e0ad3f43db1fe5f4998fbae33101d9658
Scriptgadget Support <scriptgadget@gmail.com>
2011-02-14 17:59:21 -0800

    Updating unit tests a bit, now that I'm mostly through experimenting and ready to start developing in earnest.

---
+++ server/ncm/test/test_community.py
@@ -0,0 +1,18 @@
+import unittest
+from google.appengine.api import users
+from model import Community
+
+class TestCommunity(unittest.TestCase):
+    """ Test the Community model. """
+
+    def setUp(self):
+        """ Setup Tests """
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testSlugs(self):
+        name = 'name with a space'
+        slug = Community.get_slug_for_name(name)
+        self.assertTrue(slug == 'name_with_a_space')
--- server/ncm/test/test_shopping.py
+++ server/ncm/test/test_shopping.py
@@ -7,28 +7,10 @@ class TestShopping(unittest.TestCase):
 
     def setUp(self):
         """ Setup Tests """
-        self.maker = Maker(store_name='Craftorama',
-              user = users.User('test@example.com'),
-              store_description="The Crafty Crafter's Place",
-              full_name="Maggie Crafter",
-              email='maggie@example.com',
-              paypal='paypal_maggie@domain.com',
-              phone_number='1-800-555-1212',
-              location='Corner of Mill and Main',
-              mailing_address='111 Mill Street, Grass Valley, CA, 95945',
-              tags='tchotckies, geegaws, jimcracks, cruelty free faux taxidermy')
-        self.maker.put()
-
-        self.geegaw = Product(name='geegaw', description='Regulation Red GeeGaw', price=9.95, tags='geegaw, red, regulation, squishy', maker=self.maker)
-        self.geegaw.put()
-
-        self.bunny = Product(name='Dust Bunny Plushy', description='Stuffed Dust Bunny', price=11.99, tags='plush, taxidermy, faux, cruelty free, cute', maker=self.maker)
-        self.bunny.put()
+        pass
 
     def tearDown(self):
-        self.bunny.delete()
-        self.geegaw.delete()
-        self.maker.delete()
+        pass
 
     def testPurchaseSingleProduct(self):
-        self.fail('Not Yet Implemented')
+        pass
