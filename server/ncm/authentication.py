3580700d1bb6fa5e91f91e82660c25f2619fa4cd
Scriptgadget Support <scriptgadget@gmail.com>
2011-03-14 18:59:14 -0700

    Add Licensing and Copyright info.

--- server/ncm/authentication.py
+++ server/ncm/authentication.py
@@ -1,3 +1,20 @@
+#  Copyright 2011 Bill Glover
+#
+#  This file is part of Creare.
+#
+#  Creare is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Creare is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Creare.  If not, see <http://www.gnu.org/licenses/>.
+#
 import logging
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
--- server/ncm/ipn.py
+++ server/ncm/ipn.py
@@ -1,4 +1,21 @@
 # !/usr/bin/env python
+#  Copyright 2011 Bill Glover
+#
+#  This file is part of Creare.
+#
+#  Creare is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Creare is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Creare.  If not, see <http://www.gnu.org/licenses/>.
+#
 # A simple PayPal IPN Handler
 # modified to handle chained payments
 # Based in part on an example by George Kyaw Kyaw Naing at http://learnbysoft.blogspot.com/2010/09/paypal-ipn-google-app-engine-python.html
